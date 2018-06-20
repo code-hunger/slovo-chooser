bf6a60b8672adc306cb6f6989804a225035f8362
Code Hunger <beinghavingbreackfast@gmail.com>
2018-06-20 14:36:30 +0300

    Completely decouple the core UnknownWordList from redux

--- src/containers/UnknownWordsList.tsx
+++ src/containers/UnknownWordsList.tsx
@@ -3,17 +3,11 @@ import { WordCollector } from "../views/WordCollector";
 import store, { State, WordAction } from "../store";
 import { Word, NumberedWord, NumberedWordView } from "../views/Word";
 import { Dispatch, connect } from "react-redux";
-import { KeyboardSelectableContainer } from "./NumberSelectableContainer";
-import * as _ from "lodash";
-import { TextClickStrategy } from "../TextClickStrategies";
-
-type Props = PropsFromState &
-  PropsFromDispatch & {
-    tabIndex?: number;
-  };
+import UnknownWordList from "../views/UnknownWordList";
 
 interface PropsFromState {
   words: NumberedWord[];
+  wordType: React.ComponentClass | React.StatelessComponent;
 }
 
 interface PropsFromDispatch {
@@ -36,52 +30,14 @@ const MarkedWord = connect<Word, null, { index: number }>(
   markedWordStateToProps
 )(NumberedWordView);
 
-export class UnknownWordList extends React.PureComponent<Props> {
-  private clickStrategy: TextClickStrategy;
-
-  constructor(props: Props) {
-    super(props);
-    this.onWordClick = this.onWordClick.bind(this);
-    this.clickStrategy = {
-      onWordClick: this.onWordClick,
-      onContextMenu: _.stubTrue
-    };
-    this.clickStrategy.onWordClick.bind(this.clickStrategy);
-    this.clickStrategy.onContextMenu.bind(this.clickStrategy);
-  }
-
-  onWordClick(wordId: number) {
-    this.props.onWordClick(this.props.words[wordId].index);
-    return true;
-  }
-
-  render() {
-    return this.props.words.length ? (
-      <KeyboardSelectableContainer
-        elementCount={this.props.words.length}
-        onSelectElement={this.onWordClick}
-      >
-        <WordCollector
-          className="unknownWordsList"
-          wordType={MarkedWord}
-          words={this.props.words}
-          tabIndex={this.props.tabIndex}
-          clickStrategy={this.clickStrategy}
-        />
-      </KeyboardSelectableContainer>
-    ) : (
-      "No marked words"
-    );
-  }
-}
-
 export default connect<
   PropsFromState,
   PropsFromDispatch,
   { tabIndex?: number },
   State
 >(
-  ({ cardState, words }) => ({
+  ({ cardState, words }: State) => ({
+    wordType: MarkedWord,
     words: cardState.words.marked.map((wordId, i) => ({
       ...words[wordId],
       index: i
---
+++ src/views/UnknownWordList.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { stubTrue } from "lodash";
+
+import { TextClickStrategy } from "../TextClickStrategies";
+import { Word, NumberedWord, NumberedWordView } from "./Word";
+import { WordCollector } from "./WordCollector";
+import { KeyboardSelectableContainer } from "../containers/NumberSelectableContainer";
+
+type Props = {
+  tabIndex?: number;
+  words: NumberedWord[];
+  onWordClick: (id: number) => void;
+  wordType: React.ComponentClass | React.StatelessComponent;
+};
+
+export default class UnknownWordList extends React.PureComponent<Props> {
+  private clickStrategy: TextClickStrategy;
+
+  constructor(props: Props) {
+    super(props);
+    this.onWordClick = this.onWordClick.bind(this);
+    this.clickStrategy = {
+      onWordClick: this.onWordClick,
+      onContextMenu: stubTrue
+    };
+    this.clickStrategy.onWordClick.bind(this.clickStrategy);
+    this.clickStrategy.onContextMenu.bind(this.clickStrategy);
+  }
+
+  onWordClick(wordId: number) {
+    this.props.onWordClick(this.props.words[wordId].index);
+    return true;
+  }
+
+  render() {
+    return this.props.words.length ? (
+      <KeyboardSelectableContainer
+        elementCount={this.props.words.length}
+        onSelectElement={this.onWordClick}
+      >
+        <WordCollector
+          className="unknownWordsList"
+          wordType={this.props.wordType}
+          words={this.props.words}
+          tabIndex={this.props.tabIndex}
+          clickStrategy={this.clickStrategy}
+        />
+      </KeyboardSelectableContainer>
+    ) : (
+      "No marked words"
+    );
+  }
+}
