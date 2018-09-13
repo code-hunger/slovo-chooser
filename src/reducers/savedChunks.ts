82e52237b0d0fbbe0d67f148efd37e3338cf9cb6
Code Hunger <beinghavingbreackfast@gmail.com>
2018-09-13 15:46:40 +0300

    Extract saveWord out of savedChunksReducer for easier future isolation

--- src/reducers/savedChunks.ts
+++ src/reducers/savedChunks.ts
@@ -1,6 +1,6 @@
-import { WordAction } from "../store"
+import { WordAction } from "../store";
 import update from "immutability-helper";
-import { without, isUndefined } from "lodash"
+import { without, isUndefined } from "lodash";
 
 export interface SavedWord {
   readonly word: string;
@@ -14,32 +14,49 @@ export interface SavedChunks {
   };
 }
 
-export function savedChunksReducer(savedChunks: SavedChunks = {}, action: WordAction) {
+function saveWord(
+  savedChunks: SavedChunks,
+  sourceId: string,
+  chunkId: number,
+  obj: SavedWord
+) {
+  if (isUndefined(savedChunks[sourceId]))
+    return update(savedChunks, {
+      $merge: {
+        [sourceId]: { [chunkId]: [obj] }
+      }
+    });
+
+  if (isUndefined(savedChunks[sourceId][chunkId]))
+    return update(savedChunks, {
+      [sourceId]: {
+        $merge: {
+          [chunkId]: [obj]
+        }
+      }
+    });
+
+  return update(savedChunks, {
+    [sourceId]: {
+      [chunkId]: {
+        $push: [obj]
+      }
+    }
+  });
+}
+
+export function savedChunksReducer(
+  savedChunks: SavedChunks = {},
+  action: WordAction
+) {
   switch (action.type) {
     case "SAVE_WORD":
-      const sourceId = action.textSourceId,
-        chunkId = action.chunkId;
-      if (isUndefined(savedChunks[sourceId])) {
-        savedChunks = update(savedChunks, {
-          $merge: {
-            [sourceId]: { [chunkId]: [] }
-          }
-        });
-      } else if (isUndefined(savedChunks[sourceId][chunkId]))
-        savedChunks = update(savedChunks, {
-          [sourceId]: {
-            $merge: {
-              [chunkId]: []
-            }
-          }
-        });
-      return update(savedChunks, {
-        [sourceId]: {
-          [chunkId]: {
-            $push: [action.obj]
-          }
-        }
-      });
+      return saveWord(
+        savedChunks,
+        action.textSourceId,
+        action.chunkId,
+        action.obj
+      );
     case "REMOVE_LOCAL_TEXT_SOURCE":
       return update(savedChunks, {
         $unset: [action.sourceIndex.id]
