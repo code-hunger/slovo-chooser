215f5863988ef853c71a7e1ca7c5590e21ce4816
Code Hunger <beinghavingbreackfast@gmail.com>
2018-09-19 12:56:47 +0300

    Fix the App.test.tsx to wrap App in a store Provider

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,8 +1,16 @@
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
-import App from '../App';
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import App from "../App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div') as HTMLElement;
-  ReactDOM.render(<App />, div);
+import { Provider } from "react-redux";
+import store from "../../src/store";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div") as HTMLElement;
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
 });
