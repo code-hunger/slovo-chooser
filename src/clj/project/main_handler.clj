3aa978237c040b520fafdb558d43887f7aaf7d88
Code Hunger <beinghavingbreackfast@gmail.com>
2018-10-08 00:54:58 +0300

    Wrap proper middleware based on prod/dev settings

--- src/clj/project/main_handler.clj
+++ src/clj/project/main_handler.clj
@@ -3,6 +3,7 @@
             [compojure.route :refer [not-found resources]]
             [hiccup.page :refer [include-js include-css html5]]
             [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [config.core :refer [env]]
             [prone.middleware :refer [wrap-exceptions]]
             [ring.middleware.reload :refer [wrap-reload]]))
 
@@ -14,6 +15,8 @@
 
 (defn wrap-middleware-prod [handler]
   (wrap-defaults handler site-defaults))
+
+(def wrap-middleware (if (env :dev) wrap-middleware-dev wrap-middleware-prod))
 
 (def mount-target
   [:div#app
