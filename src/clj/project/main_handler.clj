1f9be2ff29886e67b4d97fa3408c55fb8e46566c
Code Hunger <beinghavingbreackfast@gmail.com>
2018-10-08 00:44:27 +0300

    Add ring server, make it work.
    Run shadow-cljs clj-repl
    Then (use 'project.main_handler :reload)
    Then (def server (run-jetty #'handler {:port 3001 :join? false}))
    
    Mostly copied from the default lein configuration boilerplate

---
+++ src/clj/project/main_handler.clj
@@ -0,0 +1,39 @@
+(ns project.main_handler
+  (:require [compojure.core :refer [GET defroutes]]
+            [compojure.route :refer [not-found resources]]
+            [hiccup.page :refer [include-js include-css html5]]
+            [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [prone.middleware :refer [wrap-exceptions]]
+            [ring.middleware.reload :refer [wrap-reload]]))
+
+(defn wrap-middleware-dev [handler]
+  (-> handler
+      (wrap-defaults site-defaults)
+      wrap-exceptions
+      wrap-reload))
+
+(defn wrap-middleware-prod [handler]
+  (wrap-defaults handler site-defaults))
+
+(def mount-target
+  [:div#app
+      [:h3 "ClojureScript <font color=red>has</font> not been compiled!"]
+      [:p "please run "
+       [:b "lein figwheel"]
+       " in order to start the compiler"]])
+
+(defn loading-page []
+  (html5
+    [:body {:class "body-container"}
+     mount-target
+     (include-js "/js/app.js")]))
+
+
+(defroutes routes
+  (GET "/" [] (loading-page))
+  (GET "/about" [] (loading-page))
+  
+  (resources "/")
+  (not-found "Not Found"))
+
+(def handler (wrap-middleware-prod #'routes))
