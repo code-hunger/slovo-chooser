5ee9d22103cf778e5156559d77a5d0e7801cb3e5
Code Hunger <beinghavingbreackfast@gmail.com>
2018-10-15 09:11:06 +0300

    Move the `value` ratom inside KF's render() to allow multiple instances

--- src/cljs/project/keyboardFocusable.cljs
+++ src/cljs/project/keyboardFocusable.cljs
@@ -1,18 +1,16 @@
 (ns project.keyboardFocusable
   (:require [reagent.core :as r]))
 
-(def value (r/atom 0))
-
 (def KeyboardSelectableContainer
   (r/create-class
     {:reagent-render
      (fn [props]
-       [:div 
-        {:style {:position 'relative}
-         :on-key-down #(swap! value ((:handler props)  
-                                     @value 
-                                     (:elementCount props)  
-                                     %
-                                     (:onSelectElement props)))}
-        (if (> @value 0) [:div#wordNumberTyped "Typing: " @value])
-        (:children props)])}))
+       (r/with-let [value (r/atom 0)]
+         [:div 
+          {:style {:position 'relative}
+           :on-key-down #(swap! value (:handler props)
+                                (:elementCount props)  
+                                %
+                                (:onSelectElement props))}
+          (if (> @value 0) [:div#wordNumberTyped "Typing: " @value])
+          (:children props)]))}))
