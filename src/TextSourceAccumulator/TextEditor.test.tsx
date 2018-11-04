67791772aff05cdcaf357af2f1dcb5684e35ed1e
Code Hunger <beinghavingbreackfast@gmail.com>
2018-11-04 19:04:37 +0200

    Fix TextEditor test to work with the CLJS component

--- src/TextSourceAccumulator/TextEditor.test.tsx
+++ src/TextSourceAccumulator/TextEditor.test.tsx
@@ -1,11 +1,13 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
-//import TextEditor from "./TextEditor";
+import { TextEditor } from "shadow-cljs/project.TextEditor";
 import { UnknownWordSelector } from "../TextClickStrategies";
 import { mount } from "enzyme";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { reducers } from "../store";
+
+import TextWord from "./EnhancedTextWord";
 
 it("teditor word click works", () => {
   const onWordClick = jest.fn(),
@@ -14,22 +16,24 @@ it("teditor word click works", () => {
   const words = [{ index: 0, word: "word", classNames: [] }];
   const store = createStore(reducers, { words });
 
-  //const textEditor = mount(
-    //<Provider store={store}>
-      //<TextEditor
-        //className=""
-        //tabIndex={0}
-        //emptyText=""
-        //onWordClick={onWordClick}
-        //onContextMenu={onContextMenu}
-      ///>
-    //</Provider>
-  //);
+  const textEditor = mount(
+    <Provider store={store}>
+      <TextEditor
+        className=""
+        tabIndex={0}
+        emptyText=""
+        onWordClick={onWordClick}
+        onContextMenu={onContextMenu}
+        words={words}
+        wordType={TextWord}
+      />
+    </Provider>
+  );
 
-  //textEditor
-    //.find(".word")
-    //.first()
-    //.simulate("click");
+  const wordElements = textEditor.find(".word");
+  expect(wordElements).toHaveLength(words.length)
 
+  wordElements.first().simulate("click");
   expect(onWordClick).toBeCalledWith(0);
+
 });
