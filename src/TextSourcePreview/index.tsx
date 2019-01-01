3538e8dcf902c21e98064a6daa5c9e4e890ee285
Code Hunger <beinghavingbreackfast@gmail.com>
2019-01-01 14:43:51 +0200

    Restrict the preview items visible to a fixed number around the current

--- src/TextSourcePreview/index.tsx
+++ src/TextSourcePreview/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
+import { Maybe } from "monet";
 
 interface Props {
   text: string[];
@@ -11,12 +12,38 @@ interface Props {
   paperClassName: string;
 }
 
-export default class TextSourcePreviewer extends React.PureComponent<Props> {
+interface State {
+  prevShownCount: number;
+  nextShownCount: number;
+}
+
+const max = (a, b) => (a > b ? a : b);
+const equals = a => b => a == b;
+
+export default class TextSourcePreviewer extends React.PureComponent<
+  Props,
+  State
+> {
+  state = { prevShownCount: 3, nextShownCount: 3 };
+
+  currentChunkIndex = () =>
+    Maybe.fromNull(this.props.currentChunkId).map(
+      n => n - this.props.firstChunkId
+    );
+
+  firstShown = () =>
+    this.currentChunkIndex()
+      .map(current => max(current - this.state.prevShownCount, 0))
+      .orJust(0);
+
   renderChunk = (chunk: string, id: number) => {
-    const isCurrent = this.props.firstChunkId + id == this.props.currentChunkId;
+    const isCurrent = this.currentChunkIndex()
+      .map(equals(this.firstShown() + id))
+      .orJust(false);
+
     return (
-      <p style={{ color: isCurrent ? "red" : "inherit" }}>
-        {id + this.props.firstChunkId}: {chunk}
+      <p style={{ color: isCurrent ? "red" : "inherit" }} key={id}>
+        {id + this.firstShown() + 1}: {chunk}
       </p>
     );
   };
@@ -24,10 +51,14 @@ export default class TextSourcePreviewer extends React.PureComponent<Props> {
   render() {
     if (!this.props.text.length || !this.props.currentChunkId) return null;
 
+    const current: number = this.props.currentChunkId - this.props.firstChunkId;
+
     return (
       <Paper className={this.props.paperClassName}>
         <Typography variant="headline">Text source preview</Typography>
-        {this.props.text.map(this.renderChunk)}
+        {this.props.text
+          .slice(this.firstShown(), current + this.state.nextShownCount + 1)
+          .map(this.renderChunk)}
       </Paper>
     );
   }
