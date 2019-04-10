2e7a787b5dd6e24ec239284163529e9a2df31e9a
Code Hunger <beinghavingbreackfast@gmail.com>
2019-04-10 20:24:54 +0300

    Finally, MaterialUI in the Cljs part is possible
    More than 2 hours later I made it work.
    
    --- also, improved UnknownWordsList's render method's clarity

--- src/cljs/project/UnknownWordList.cljs
+++ src/cljs/project/UnknownWordList.cljs
@@ -2,25 +2,26 @@
   (:require [reagent.core :as r]
              ["shadow-cljs/project.keyboardFocusable" :refer (KeyboardSelectableContainer)]
              ["shadow-cljs/project.WordCollector" :refer (WordCollector)]
-             ["shadow-cljs/project.TEKeyboardHandler" :refer (handler)]))
+             ["shadow-cljs/project.TEKeyboardHandler" :refer (handler)]
+             ["@material-ui/core/Typography"  :default Typography']))
+
+(def Typography (r/adapt-react-class Typography'))
 
 (def UnknownWordList
   (r/create-class
     {:display-name 'UnknownWordList
      :reagent-render
-      (fn [props]
-        (let [words (:words props)]
-          (if (empty? words)
-            "No marked words"
-            [KeyboardSelectableContainer
-             {:elementCount (count words)
-              :onSelectElement (props :onWordClick)
-              :handler handler}
-             [WordCollector
-              {:className "unknownWordsList"
-               :wordType (:wordType props)
-               :words words
-               :tabIndex (:tabIndex props)
-               :onWordClick #((:onWordClick props) (.-index (nth words %)))
-               :onContextMenu (constantly true)}]
-             ])))}))
+     (fn [{:keys [words onWordClick wordType tabIndex]}]
+       (if (empty? words)
+         [Typography {:variant "subheading"} "No marked words."]
+         [KeyboardSelectableContainer
+          {:elementCount (count words)
+           :onSelectElement onWordClick
+           :handler handler}
+          [WordCollector
+           {:className "unknownWordsList"
+            :wordType wordType 
+            :words words
+            :tabIndex tabIndex
+            :onWordClick #(onWordClick (.-index (nth words %)))
+            :onContextMenu (constantly true)}]]))}))
