d3490bcae2ced143f0d61544e19dbddd4092e95a
Code Hunger <beinghavingbreackfast@gmail.com>
2019-04-10 21:06:30 +0300

    Transform the CLJS-inner components into Form-1 components
    They're no longer needed by the TypeScript part so they can be safely
    simplified

--- src/cljs/project/keyboardFocusable.cljs
+++ src/cljs/project/keyboardFocusable.cljs
@@ -2,13 +2,11 @@
   (:require [reagent.core :as r]
             [project.TEKeyboardHandler :refer (handler)]))
 
-(def KeyboardSelectableContainer
-  (r/create-class
-    {:reagent-render
-     (fn [{:keys [elementCount onSelectElement children]} children2]
-       (r/with-let [value (r/atom 0)]
-         [:div 
-          {:style {:position 'relative}
-           :on-key-down #(swap! value handler elementCount % onSelectElement)}
-          (if (> @value 0) [:div#wordNumberTyped "Typing: " @value])
-          (or children children2)]))}))
+(defn KeyboardSelectableContainer
+  [{:keys [elementCount onSelectElement children]} children2]
+  (r/with-let [value (r/atom 0)]
+    [:div 
+     {:style {:position 'relative}
+      :on-key-down #(swap! value handler elementCount % onSelectElement)}
+     (if (> @value 0) [:div#wordNumberTyped "Typing: " @value])
+     (or children children2)]))
